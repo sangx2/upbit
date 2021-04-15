ac1044e71000121322f89834ad9e20989744c5b3
sangx2 <sangrae@hancomwith.com>
2021-04-16 00:03:34 +0900

    remove test keys

--- upbit_test.go
+++ upbit_test.go
@@ -7,8 +7,8 @@ import (
 
 // fix me
 var (
-	accessKey = "8nSmG42bkK7CmgN8L23zARByPLy5c538PkhkgsOf"
-	secretKey = "Z8aX43xRHcNXg2e2Aw8ijHzLW9n6WlNy5K5XENIq"
+	accessKey = ""
+	secretKey = ""
 
 	marketID = "KRW-ETH"
 	currency = "ETH"
