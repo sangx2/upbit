aecaa6fe8b062a5de7d2f31ef1cc40d60ddaa075
sangx2 <sangrae@hancomwith.com>
2021-10-15 01:24:36 +0900

    Withdrarw 출금 구조체 버그 수정

--- model/exchange/withdraw/withdraw.go
+++ model/exchange/withdraw/withdraw.go
@@ -7,16 +7,17 @@ import (
 
 // Withdraw : 출금 정보
 type Withdraw struct {
-	Type      string `json:"type"`
-	UUID      string `json:"uuid"`
-	Currency  string `json:"currency"`
-	TxID      string `json:"txid"`
-	State     string `json:"state"`
-	CreatedAt string `json:"create_at"`
-	DoneAt    string `json:"done_at"`
-	Amount    string `json:"amount"`
-	Fee       string `json:"fee"`
-	KrwAmount string `json:"krw_amount"`
+	Type            string `json:"type"`
+	UUID            string `json:"uuid"`
+	Currency        string `json:"currency"`
+	TxID            string `json:"txid"`
+	State           string `json:"state"`
+	CreatedAt       string `json:"create_at"`
+	DoneAt          string `json:"done_at"`
+	Amount          string `json:"amount"`
+	Fee             string `json:"fee"`
+	KrwAmount       string `json:"krw_amount"`
+	TransactionType string `json:"transaction_type"`
 }
 
 func WithdrawFromJSON(r io.Reader) (*Withdraw, error) {
